4a0213c3f723cb78bb7d169b4ddeed9f3cd9d6e8
Bernd Müller <github@muellerbernd.de>
2023-12-07 21:02:12 +0100

    day05: task2, mostly stolen

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,4 +1,7 @@
+use std::collections::HashSet;
 use std::fs::read_to_string;
+use std::iter::zip;
+use std::iter::FromIterator;
 
 fn parse_input(raw_input: &String) -> Vec<Vec<Vec<usize>>> {
     raw_input
@@ -22,9 +25,10 @@ fn task01(puzzle_input: &Vec<Vec<Vec<usize>>>) -> usize {
     let mut locations: Vec<usize> = Vec::new();
     for seed in seeds {
         let mut scratch_var = seed;
-        for mappings in maps {
-            for m in mappings {
-                let (dest_start, src_start, range): (usize, usize, usize) = (m[0], m[1], m[2]);
+        for map in maps {
+            for line in map {
+                let (dest_start, src_start, range): (usize, usize, usize) =
+                    (line[0], line[1], line[2]);
                 if scratch_var >= src_start && scratch_var < (src_start + range) {
                     scratch_var = dest_start + (scratch_var - src_start);
                     break;
@@ -34,6 +38,64 @@ fn task01(puzzle_input: &Vec<Vec<Vec<usize>>>) -> usize {
         locations.push(scratch_var);
     }
     *locations.iter().min().unwrap()
+}
+
+fn task02(puzzle_input: &Vec<Vec<Vec<usize>>>) -> usize {
+    let parsed_seeds: Vec<usize> = puzzle_input[0][0].clone();
+    let mut scratchpad: Vec<(usize, usize)> = (0..(parsed_seeds.len() / 2))
+        .map(|i| {
+            (
+                parsed_seeds[i * 2],
+                parsed_seeds[(i * 2) + 1] + parsed_seeds[i * 2],
+            )
+        })
+        .collect();
+    dbg!(scratchpad.clone());
+    let maps = &puzzle_input[1..];
+    for map in maps {
+        let mut counter = 0;
+        while counter < scratchpad.len() {
+            for line in map {
+                let (dest_start, src_start, range): (usize, usize, usize) =
+                    (line[0], line[1], line[2]);
+                let (low_is_in, up_is_in, is_around) =
+                    between(scratchpad[counter], src_start, range);
+                if low_is_in && up_is_in {
+                    scratchpad[counter] = (
+                        (scratchpad[counter].0 - src_start) + dest_start,
+                        (scratchpad[counter].1 - src_start) + dest_start,
+                    );
+                    break;
+                } else if low_is_in {
+                    scratchpad.push((src_start + range, scratchpad[counter].1));
+                    scratchpad[counter] = (
+                        (scratchpad[counter].0 - src_start) + dest_start,
+                        dest_start + range,
+                    );
+                    break;
+                } else if up_is_in {
+                    scratchpad.push((scratchpad[counter].0, src_start));
+                    scratchpad[counter] =
+                        (dest_start, (scratchpad[counter].1 - src_start) + dest_start);
+                    break;
+                } else if is_around {
+                    scratchpad.push((scratchpad[counter].0, src_start));
+                    scratchpad.push((src_start + range, scratchpad[counter].1));
+                    scratchpad[counter] = (dest_start, dest_start + range);
+                    break;
+                }
+            }
+            counter += 1;
+        }
+    }
+    *scratchpad.iter().map(|(v, _)| v).min().unwrap()
+}
+fn between(v: (usize, usize), lower: usize, range: usize) -> (bool, bool, bool) {
+    (
+        v.0 >= lower && v.0 < lower + range,
+        v.1 > lower && v.1 <= lower + range,
+        v.0 < lower && v.1 > lower + range,
+    )
 }
 
 fn main() {
@@ -47,5 +109,6 @@ fn main() {
     let parsed_input = parse_input(&raw_input);
     let task01_solution = task01(&parsed_input);
     println!("task01 solution = {}", task01_solution);
-    // task02(&puzzle_input);
+    let task02_solution = task02(&parsed_input);
+    println!("task02 solution = {}", task02_solution);
 }
