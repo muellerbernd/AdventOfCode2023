16e4458da1601813fcd256b1cb0ec42f1a5bcd2b
Bernd Müller <github@muellerbernd.de>
2023-12-13 23:11:09 +0100

    day10: parse input

---
+++ day10/src/main.rs
@@ -0,0 +1,28 @@
+use std::fs::read_to_string;
+
+fn parse_input(raw_input: &String) {
+    println!("{}", raw_input);
+    let grid: Vec<Vec<char>> = raw_input.lines().map(|l| l.chars().collect()).collect();
+    println!("grid {:?}", grid);
+    let mut start_xy: (usize, usize) = (0, 0);
+    // let start_xy = grid.iter().enumerate().map(|(r,l)| l.iter().enumerate().);
+    for (row, line) in grid.iter().enumerate() {
+        for (col, c) in line.iter().enumerate() {
+            if *c == 'S' {
+                start_xy = (col,row);
+                break;
+            }
+        }
+    }
+    println!("start_xy {:?}", start_xy);
+}
+
+fn main() {
+    // let file_path = "../inputs/aoc_10.txt";
+    let file_path = "sample_input.txt";
+
+    let raw_input: String =
+        read_to_string(file_path).expect("Should have been able to read the file");
+    parse_input(&raw_input);
+    // let camel_card_to_bid: Vec<(Vec<usize>, i32)> = parse_input(raw_input.clone());
+}
