aa575a8994d5559e31b688e1b3780b4007298764
Bernd Müller <github@muellerbernd.de>
2023-12-15 12:33:33 +0100

    day10: go through possible starting positions

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,21 +1,18 @@
 use std::fs::read_to_string;
 
 fn parse_input(raw_input: &String) -> (Vec<Vec<char>>, (usize, usize)) {
-    println!("{}", raw_input);
-    let grid: Vec<Vec<char>> = raw_input.lines().map(|l| l.chars().collect()).collect();
-    println!("grid {:?}", grid);
-    let mut start_xy: (usize, usize) = (0, 0);
+    let field: Vec<Vec<char>> = raw_input.lines().map(|l| l.chars().collect()).collect();
+    let mut start_point: (usize, usize) = (0, 0);
     // let start_xy = grid.iter().enumerate().map(|(r,l)| l.iter().enumerate().);
-    for (row, line) in grid.iter().enumerate() {
+    for (row, line) in field.iter().enumerate() {
         for (col, c) in line.iter().enumerate() {
             if *c == 'S' {
-                start_xy = (col, row);
+                start_point = (col, row);
                 break;
             }
         }
     }
-    println!("start_xy {:?}", start_xy);
-    (grid, start_xy)
+    (field, start_point)
 }
 
 fn main() {
@@ -24,7 +21,24 @@ fn main() {
 
     let raw_input: String =
         read_to_string(file_path).expect("Should have been able to read the file");
-    let (grid, start_xy): (Vec<Vec<char>>, (usize, usize)) = parse_input(&raw_input);
-    println!("{:?} {:?}", grid, start_xy);
+    let (field, start_point): (Vec<Vec<char>>, (usize, usize)) = parse_input(&raw_input);
+    println!("{:?} {:?}", field, start_point);
     // let camel_card_to_bid: Vec<(Vec<usize>, i32)> = parse_input(raw_input.clone());
+    let possible_startings = [
+        (start_point.0 + 1, start_point.1),
+        (start_point.0 - 1, start_point.1),
+        (start_point.0, start_point.1 + 1),
+        (start_point.0, start_point.1 - 1),
+    ];
+    for (col, row) in possible_startings {
+        let tile: char = match field.get(row) {
+            Some(r) => match r.get(col) {
+                Some(c) => *c,
+                None => continue,
+            },
+            None => continue,
+        };
+        println!("tile {:?}", tile);
+        // TODO: implement function that follows the pipe
+    }
 }
